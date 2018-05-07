b6aba9acfd47aa9ed75395461a1c47a48ae88298
Mayuran <mayuran29@gmail.com>
2018-05-07 14:57:02 -0400

    commit

---
+++ AutomationFramework/Driver/Driver.cs
@@ -0,0 +1,70 @@
+﻿
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Remote;
+using System.IO;
+using System.Reflection;
+using OpenQA.Selenium.IE;
+
+namespace AcmUITest.Pages
+{
+    public class Driver
+    {
+        #region Constructors
+
+       
+        #endregion
+
+        public static IWebDriver myDriver;
+
+
+
+
+        #region Components
+        public static int ProcessID { get; set; }
+        #endregion
+
+        #region Methods
+
+        public static IWebDriver Initialize()
+        {
+            InternetExplorerOptions IEcaps = new InternetExplorerOptions();
+            IEcaps.IgnoreZoomLevel = true;
+            IEcaps.IntroduceInstabilityByIgnoringProtectedModeSettings = true;
+            myDriver = new InternetExplorerDriver(IEcaps);
+            myDriver.Manage().Window.Maximize();
+
+
+            return myDriver;
+            
+        }
+
+        public static void Close()
+        {
+            myDriver.Close();
+            myDriver.Quit();
+        }
+
+
+       
+
+
+        #endregion
+
+        #region Validations
+
+        #endregion
+
+
+    }
+}
---
+++ AutomationFramework/TestMethod/UIValidation.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using AcmUITest.Pages;
+
+namespace AutomationFramework.TestMethod
+{
+    [TestClass]
+    public class UIValidation
+    {
+       
+
+            public static IWebDriver myDriver;
+                       
+            
+            #region Initialize
+
+            [TestInitialize]
+            public void Setup()
+            {
+                Driver mySample = new Driver();
+                myDriver = Driver.Initialize();
+            }
+
+            #endregion Initialize
+            [TestMethod]
+            
+            public void NaviagteToHomePage_ClickToTourButton_EnterEmailAndClickSubmitButton()
+            {
+            myDriver.Navigate().GoToUrl("http://www.aveasolutions.com/");
+
+            
+            }
+
+
+            //Closing the browser   
+            [TestCleanup]
+            public void CloseDown()
+            {
+                myDriver.Close();
+                myDriver.Quit();
+               
+            }
+        }
+    }
+
